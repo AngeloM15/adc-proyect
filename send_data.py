423983241b8635e4b5c62daef7c685ba0cee0a8b
Angelo M <36874846+AngeloM15@users.noreply.github.com>
2022-07-19 13:38:05 -0300

    feat: Added script to send data independently

---
+++ send_data.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from datetime import datetime
+import time
+import json
+
+HOME = os.path.expanduser('~')+"/potenciostato-project"
+
+sys.path.append(f"{HOME}")
+
+from libs.libdata import *
+from libs.libutils import *
+
+
+def main():
+
+    libutils = Libutils()
+    libconversor = Libconversor()
+
+    # Read config.json to get parameters of configuration
+    with open(f"{HOME}/config/config.json") as file:
+        config_json = json.load(file)
+        sender_config = config_json["SENDER_PROCESS"]
+
+    # Set API
+    libutils.set_channel(sender_config["CHANNEL_ID"],sender_config["WRITE_KEY"])
+    
+    # Set period
+    every = sender_config["PERIOD"]
+
+    # Define data paths
+    yearmonth = datetime.now().strftime('%Y-%m')
+    libconversor.temporal_file_name = f"{HOME}/data/tem-data-{yearmonth}.csv"
+    libconversor.load_data(libconversor.temporal_file_name)
+
+    # Send data
+    for i,row in libconversor.signal_df.iterrows():
+        #libutils.write_data(row["DAC"],row["ADC"])
+        print(row)
+        time.sleep(every)
+
+
+if __name__ == "__main__":
+    main()
